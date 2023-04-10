22a7518e3488c01e7f61783c84e73e29b0939b2e
Resch-Gabriel-Z <reschzev@outlook.com>
2023-04-10 18:16:40 +0200

    Mainly some Structural things like defining/fleshing out functions and adding TODOs

--- DDQN scripts/Agent_Training.py
+++ DDQN scripts/Agent_Training.py
@@ -1,18 +1,12 @@
-# TODO: define load function that searches for a file and loads the data in the premade variables
-# TODO: define those variables beforehand
-# TODO: define save functions that saves the file
-# TODO: define learning function
-# TODO: define an update target function
-
-# TODO: create an Agent and an online network (and initialize weights)
-# TODO: create the Memory
-# TODO: create optimizer and loss function
 # TODO: create some variables for logging and such
 
 import gym
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
+
+import os
 
 from Agent import Agent
 from Neural_Network import DQN
@@ -21,11 +15,26 @@ from Hyperparameters import hyperparameters
 from Replay_Memory import ReplayMemory
 
 # A function to load a model if existing
-def load_model_dict(path,name):
+def load_model_dict(path,name,**kwargs):
+    # TODO: define the kwargs and load model (currently network state dicts, starting point, optimizer_dict, total steps)
+    policy_net_load = kwargs['policy_state_dict']
+    online_net_load = kwargs['online_state_dict']
+    start_load = kwargs['start']
+    optimizer_load = kwargs['optimizer_state_dict']
+    total_steps_load = kwargs['total_steps']
+
+    if os.path.exists(path+'/'+name):
+        checkpoint = torch.load(path+'/'+name)
     pass
 
 # A function to save a model
-def save_model_dict(path,name):
+def save_model_dict(path,name, **kwargs):
+    # TODO: as above
+    policy_net_save = kwargs['policy_state_dict']
+    online_net_save = kwargs['online_state_dict']
+    start_save = kwargs['start']
+    optimizer_save = kwargs['optimizer_state_dict']
+    total_steps_save = kwargs['total_steps']
     pass
 
 # A function to create and preprocess the Environment
@@ -35,10 +44,19 @@ def environment_maker(game):
     return env
 
 # Learning function that updates the policy network
-def Agent_learning(batch_size):
-    pass
+def Agent_learning(batch_size,gamma,**kwargs):
+    memory_l = kwargs['memory']
+    agent_l = kwargs['agent']
+    online_l = kwargs['online_network']
+    optimizer_l = kwargs['optimizer']
+    loss_func_l = kwargs['loss_function']
 
-def update_online_network():
+    if len(memory_l) < 1000:
+        return
+
+    # TODO: get the information from the memory in tensors of shape (batch_size,in_channels)
+    # TODO: compute targets with the online network and the formula given in paper
+    # TODO: compute loss and optimize policy network
     pass
 
 # Hyperparameters
@@ -49,14 +67,14 @@ agent_hyperparameters = [hyperparameters['initial_eps'],hyperparameters['final_e
 env = environment_maker("ALE/Breakout-v5")
 
 # Create the Agent and the online Network
-policy_net = Agent(*agent_hyperparameters,env.observation_space.shape,env.action_space.shape)
+agent = Agent(*agent_hyperparameters, env.observation_space.shape, env.action_space.shape)
 online_net = DQN(env.observation_space.shape,env.action_space.shape)
 
 # Initialize the weights of the online net with the policy nets weights
-online_net.load_state_dict(policy_net.policy_net.load_state_dict())
+online_net.load_state_dict(agent.policy_net.load_state_dict())
 
 # Initialize optimizer, loss function
-optimizer = optim.SGD(policy_net.policy_net.parameters(),hyperparameters['learning_rate'])
+optimizer = optim.SGD(agent.policy_net.parameters(), hyperparameters['learning_rate'])
 loss_function = nn.MSELoss()
 
 # Initialize Memory
